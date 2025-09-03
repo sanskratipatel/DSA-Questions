4ed1fa11d5d8026ad616be9aa736b3d8a4c3daa8
Sanskrati Patel <patelsanskrati@gmail.com>
2025-09-03 09:55:33 +0530

    Add files

--- Linklist/3middleofLinkelist.py
+++ Linklist/3middleofLinkelist.py
@@ -1,53 +1,52 @@
 class Node : 
-    def __init__(self ,val):
-        self.val = val 
+    def __init__ (self ,val) :
+        self.val =val 
         self.next = None
-    
 
 class SLL: 
     def __init__(self):
-        self.head =None 
-    
-    def append(self,val) : 
-        newnode = Node(val) 
+        self.head = None 
+
+    def append(self,val ): 
+        newnode = Node(val)
         if self.head == None: 
-            self.head = newnode 
-        
+            self.head =newnode 
+        else: 
+            curr = self.head 
+            while curr.next is not None: 
+                curr = curr.next 
+            curr.next = newnode
+    
+    def traverse(self):
+        if self.head ==None:
+            print("SLL is empty") 
         else : 
             current = self.head 
-            while current.next is not None: 
+            while current is not None:
+                print(current.val , end = " ")
                 current = current.next  
-            current.next = newnode
-      
-    def traversal(self) : 
-        if self.head == None: 
-            print("LL is Empty") 
-        else:
-            current =  self.head 
-            while current is not None: 
-                print(current.val ,end=" ")  
-                current =current.next
+    def middleelement (self):
+        temp = self.head
+        count =0 
+        while temp is not None:
+            count = count + 1 
+            temp = temp.next
+        temp2 = self.head
+        for i in range (0,count//2): 
+            temp2 = temp2.next
+        print("middle = ",temp2 ,"value ",temp2.val)
 
-    def middle_element(self) :  
-        n =  0 
-        temp = self.head 
-        while temp is not None :  
-            n = n+1 
-            temp = temp.next 
-        temp  =self.head 
-        for i in range(0,n//2) : 
-            temp = temp.next 
-        return temp.val
 
-s1 = SLL() 
-s1.append(10) 
+s1 = SLL()
+s1.append(10)
 s1.append(20) 
-s1.append(30) 
-s1.append(40) 
-s1.append(50)
-s1.traversal() 
-print()
+s1.append(110)
+s1.append(201) 
+s1.append(105)
+s1.append(207) 
+s1.traverse() 
+s1.middleelement()
 
-print(s1.middle_element())
 
-    
+
+
