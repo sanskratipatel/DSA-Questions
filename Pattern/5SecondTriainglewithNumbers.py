310533887f8819dd43a8fcb3ccaaf6b3ae295b34
Sanskrati Patel <patelsanskrati@gmail.com>
2025-09-07 23:46:36 +0530

    Pattern Question

---
+++ Pattern/5SecondTriainglewithNumbers.py
@@ -0,0 +1,20 @@
+nums =6 
+
+for i in range(0,nums): 
+    for j in range(0,i)  :
+        print(i ,end = " ") 
+    print() 
+
+#  print(i-j)   
+# 1 
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1  
+
+# print(2*i-j ,end = " ")
+# 2 
+# 4 3
+# 6 5 4
+# 8 7 6 5
+# 10 9 8 7 6
